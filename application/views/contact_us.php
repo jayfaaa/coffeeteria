784e566df9457edb861eb02225b7b438c7e42368
it3r4-pineda <vladsggmalupet@gmail.com>
2019-03-27 17:19:31 +0800

    second commit

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -1,6 +1,6 @@
 <center>
-	<div style="margin-top: 5%;">
-	<h4> Contact Us </h4>
+	<div style="margin-top: 5%; margin-bottom: 55">
+	<h4 style="margin-bottom: 2%"> Contact Us </h4>
 		<img height="150px" style="margin-right: 5%; border-radius: 100%;" src="<?php echo base_url();?>assets/images/chris.jpg">
 		<img height="150px" style="border-radius: 100%;" src="<?php echo base_url();?>assets/images/efren.jpg">
 		<img height="150px" style="margin-left: 5%; border-radius: 100%;" src="<?php echo base_url();?>assets/images/pat.jpg">

446bc000e1070fbad38eb168409e76daa4f6b9a3
it3r4-pineda <vladsggmalupet@gmail.com>
2019-03-30 03:14:01 +0800

    images changes

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -1,8 +1,18 @@
 <center>
-	<div style="margin-top: 5%;">
-	<h4> About Us </h4>
+	<div style="margin-top: 5%; margin-bottom: 55">
+	<h4 style="margin-bottom: 2%"> About Us </h4>
+		<img height="150px" style="margin-right: 5%; border-radius: 100%;" src="<?php echo base_url();?>assets/images/jules.jpg">
 		<img height="150px" style="margin-right: 5%; border-radius: 100%;" src="<?php echo base_url();?>assets/images/chris.jpg">
-		<img height="150px" style="border-radius: 100%;" src="<?php echo base_url();?>assets/images/efren.jpg">
+		<img height="150px" style="border-radius: 100%;" src="<?php echo base_url();?>assets/images/jay.jpg">
+		<img height="150px" style="margin-left: 5%; border-radius: 100%;" src="<?php echo base_url();?>assets/images/efren.jpg">
 		<img height="150px" style="margin-left: 5%; border-radius: 100%;" src="<?php echo base_url();?>assets/images/pat.jpg">
+	</div>
+
+	<div style="margin-bottom: 55; horizontal-align: left; ">
+		<span style="  margin-right: 7%; white-space: nowrap;">Jules Cabulay</span>
+		<span style="  margin-right: 5%; white-space: nowrap;">Chris John Labanon</span>
+		<span style="  margin-left: 3%; margin-right: 3%; white-space: nowrap;">Arch Jay Jabla</span>
+		<span style="  margin-left: 5%; white-space: nowrap;">Efren Ray Pineda</span>
+		<span style="  margin-left: 7%; white-space: nowrap;">Patrick Corambao</span>
 	</div>
 </center>
